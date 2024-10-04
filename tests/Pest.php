3301d4e7553a22206ad406295eaec397c282d91f
rockblings <infor@rockblings.com>
2024-10-04 23:21:57 +0200

    updated the run test github action workflow

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,3 +5,4 @@ declare(strict_types=1);
 use CreativeCrafts\LaravelOpenidConnect\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+pest()->project()->github('CreativeCrafts/laravel-openid-connect');
