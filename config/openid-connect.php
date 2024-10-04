e019f09319ce3f20f80381ec3bbb85ab506c881f
rockblings <infor@rockblings.com>
2024-10-04 12:46:15 +0200

    initial setup

--- config/openid-connect.php
+++ config/openid-connect.php
@@ -1,6 +1,5 @@
 <?php
 
-// config for CreativeCrafts/LaravelOpenidConnect
 return [
 
 ];
--- tests/Pest.php
+++ tests/Pest.php
@@ -3,3 +3,4 @@
 use CreativeCrafts\LaravelOpenidConnect\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+pest()->project()->github('CreativeCrafts/laravel-openid-connect');
