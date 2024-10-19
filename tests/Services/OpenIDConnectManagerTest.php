8b8013679c5d50dd21b0ef86e5495dac9edd4337
rockblings <rockblings@users.noreply.github.com>
2024-10-19 07:09:38 +0000

    Fix styling

--- tests/Services/OpenIDConnectManagerTest.php
+++ tests/Services/OpenIDConnectManagerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use CreativeCrafts\LaravelOpenidConnect\Exceptions\OpenIDConnectClientException;
 use CreativeCrafts\LaravelOpenidConnect\Services\OpenIDConnectManager;
 
